15b8f008e7b561171e482715b3a13c98ff10a77c
luiscc07 <149604916+luiscc07@users.noreply.github.com>
2024-01-23 15:52:22 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,51 @@
-
+let Attacker: Sprite = null
+let Defender: Sprite = null
+controller.player1.onEvent(ControllerEvent.Connected, function () {
+    game.showLongText("SELECT CLASS", DialogLayout.Top)
+    if (controller.anyButton.isPressed()) {
+        game.showLongText("A : ATTACKER", DialogLayout.Top)
+        game.showLongText("Down : DEFENDER", DialogLayout.Top)
+        if (controller.A.isPressed()) {
+            Attacker = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, SpriteKind.Player)
+            controller.player1.moveSprite(Attacker)
+        }
+        if (controller.down.isPressed()) {
+            Defender = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, SpriteKind.Player)
+            controller.player1.moveSprite(Defender)
+        }
+    }
+})
